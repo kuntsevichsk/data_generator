51a04f96690f84319b1144426d4b06e337d5284a
kuntsevichsk <kuntsevich.s+1@gmail.com>
2016-12-07 02:00:46 +0300

    Fx belarussian yml. Rename all ymls. Add more fields to otput.

--- dataset.rb
+++ dataset.rb
@@ -1,15 +1,17 @@
 class DataSet
-  attr_accessor :first_name, :last_name, :city, :street, :street_number, :apt_number
+  attr_accessor :first_name, :last_name, :city, :postal_code, :street, :street_number, :apt_number, :phone
   def initialize(array)
     @first_name = array[0]
     @last_name = array[1]
-    @city = array[2]
-    @street = array[3]
-    @street_number = array[4]
-    @apt_number = array[5]
+    @postal_code = array[2]
+    @city = array[3]
+    @street = array[4]
+    @street_number = array[5]
+    @apt_number = array[6]
+    @phone = array[7]
   end
 
   def to_s
-    "#{first_name} #{last_name} #{city} #{street} #{street_number} #{apt_number}"
+    "#{first_name} #{last_name} г. #{city} #{postal_code} ул. #{street} #{street_number}, кв. #{apt_number} #{phone}"
   end
 end
--- generator.rb
+++ generator.rb
@@ -8,7 +8,7 @@ a = []
 time = Benchmark.realtime do
   ALLOWED_COUNTRIES = ['us', 'ru', 'by']
 
-  raise ArgumentError, 'Should be a string' unless ALLOWED_COUNTRIES.include? ARGV[0]
+  raise ArgumentError, "Should be in #{ALLOWED_COUNTRIES}" unless ALLOWED_COUNTRIES.include? ARGV[0]
   raise ArgumentError, 'Should be a number' unless ARGV[1].to_i
   raise ArgumentError, 'Should be a number' unless ARGV[2].to_i
 
@@ -17,15 +17,27 @@ time = Benchmark.realtime do
   error_count = ARGV[2].to_i
   country = ARGV[0]
 
-  data = YAML.load_file('belarus.yml')
-  first_names = YAML.load_file('first_names.yml')['names']
-  last_names = YAML.load_file('last_names.yml')['names']
+  data = YAML.load_file("#{country}/adress.yml")
+  men_first_names = YAML.load_file("#{country}/men_first_names.yml")['names']
+  women_first_names = YAML.load_file("#{country}/women_first_names.yml")['names']
+  all_names = men_first_names + women_first_names
+  last_names = YAML.load_file("#{country}/last_names.yml")['names']
+  phone_codes = YAML.load_file("#{country}/phone_codes.yml")['codes']
 
-  1.upto(all_count).each do |idx|
+  1.upto(all_count).each do |_idx|
     data.each do |city, postal_codes|
-      a << DataSet.new([first_names.sample, last_names.sample, city, postal_codes[postal_codes.keys.sample].sample, rand(50), rand(50)])
+      random_code = postal_codes.keys.sample
+      a << DataSet.new([all_names.sample,
+                        last_names.sample,
+                        city,
+                        random_code,
+                        postal_codes[random_code].sample,
+                        rand(50),
+                        rand(50),
+                        "+375-#{phone_codes.sample}-#{rand(100..999)}-#{rand(10..99)}-#{rand(10..99)}}"])
       break if a.length == all_count
     end
+    break if a.length == all_count
   end
 
 end
