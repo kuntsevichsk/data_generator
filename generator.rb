bdc0345dabba80b67557d72b1d7340a8352a1bae
kuntsevichsk <kuntsevich.s+1@gmail.com>
2016-12-07 12:02:50 +0300

    phone maker function

--- generator.rb
+++ generator.rb
@@ -22,6 +22,14 @@ time = Benchmark.realtime do
   last_names = YAML.load_file("#{country}/last_names.yml")['names']
   phone_codes = YAML.load_file("#{country}/phone_codes.yml")['codes']
 
+  def phone(country)
+    if country=='us'
+      return "+1-#{rand(200..999)}-#{rand(100..999)}-#{rand(1000..9999)}"
+    if country=="ru"
+      return "+7-#{phone_codes.sample}-#{rand(100..999)}-#{rand(10..99)}-#{rand(10..99)}"
+    if country=='by'
+      return "+375-#{phone_codes.sample}-#{rand(100..999)}-#{rand(10..99)}-#{rand(10..99)}"
+
   1.upto(all_count).each do |_idx|
     data.each do |city, postal_codes|
       random_code = postal_codes.keys.sample
@@ -32,7 +40,7 @@ time = Benchmark.realtime do
                         postal_codes[random_code].sample,
                         rand(50),
                         rand(50),
-                        "+375-#{phone_codes.sample}-#{rand(100..999)}-#{rand(10..99)}-#{rand(10..99)}"])
+                        phone(country)])
       break if a.length == all_count
     end
     break if a.length == all_count
